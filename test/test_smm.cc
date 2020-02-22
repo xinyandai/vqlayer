e3c8febf2108c76053578022532aa04ac13583ab
xinyan <381675152@qq.com>
2020-02-22 21:28:13 +0800

    Add test cases for softmax computing and gradient of softmax cross entropy.

--- test/test_smm.cc
+++ test/test_smm.cc
@@ -5,7 +5,6 @@
 #include "../include/layer.h"
 
 void dump(const SparseVector& s) {
-
   for (int i = 0; i < s.size(); ++i) {
     std::cout << s.index_[i] << " : " << s.value_[i] <<"\t";
   }
@@ -36,7 +35,6 @@ void compare(const std::string& variable, const SparseVector& s, const T* p, int
 }
 
 void test_smm_relu() {
-
   vector<T>  x_  = { 0.1357047994833403, -0.9500842332443221 };
   vector<T>  w_  = { 0.12282730752559588, 1.901416969226425, -1.573997496240008, 0.20564681374665741 };
   vector<T>  b_  = { -1.2207054473768937, 1.5676540439571955 };
@@ -64,13 +62,54 @@ void test_smm_relu() {
   compare("update_w_", update_w_, layer.weight(), I*O);
   compare("update_b_", update_b_, layer.bias(), O);
   compare("gradient_x",c_gx, gx_.data(), gx_.size());
+}
 
+void test_smm_softmax() {
+  size_type I=8, O=4;
+  vector<T>  x_  = { 1.5516870102023173, -0.9251752728867606, -0.014352775709434534,
+                     0.22134742695532325, 0.15594105122622795, 0.5030637979205353,
+                     -0.7834341624394263, 0.7741397728809003 };
+  vector<T>  w_  = { 0.18785237059264645, -0.8551286574783469, -0.3720122543464393,
+                     -1.1245439053024775, 1.1008960707603663, 1.2242894316303707,
+                     0.6083893460380334, -0.1844802709171102, 0.4925100769615539,
+                     0.07239803849750229, 0.13475540977024994, -1.5512873822380544,
+                     -1.7792852223291857, 0.7621670322758677, 1.7524477864075518,
+                     -0.528663313911014, 0.10796811613632247, -1.684173979989373,
+                     0.9861907424803545, 0.5204656840717133, -1.409598693782954,
+                     -1.1382040754746086, -0.4954915626388351, -0.297129980483365,
+                     -0.29775108503821784, 0.6222751276617549, 1.5488036759136954,
+                     2.35938171417603, 0.6042851340815848, -0.06642175572357562,
+                     1.3076468425197068, 0.36328624713534113 };
+  vector<T>  b_  = { 0.886633630580316, -0.8271605666946733,
+                     -0.182012857677164, -0.17508423942467125 };
+  vector<T>  o_  = { 0.7038258993214305, 0.009932670338585496,
+                     0.2588713071665085, 0.027370123173475553 };
+
+
+  vector<size_type >  y_  = { 3 };
+  vector<T>  g_  = { 0.7038258993214305, 0.009932670338585496, 0.2588713071665085,
+                     -0.9726298768265245 };
+  T loss_ = 3.59830325563001;
+
+  Layer layer(I, O, Activation::SoftMax);
+  layer.initialize(w_, b_);
+  SparseVector x = x_;
+  T loss;
+
+  SparseVector c_o = layer.forward(x);
+  SparseVector g = SoftMaxCrossEntropy::compute(c_o, y_, &loss);
+
+  compare("softmax forward", c_o, o_.data(), o_.size());
+
+  compare("cross entropy gradient", g, g_.data(), g_.size());
+  std::cout <<  (( std::abs(loss - loss_) < 0.0001)? "[PASS]":"[FAIL]")
+            << " Cross Entropy Loss" << std::endl;
 
 
 }
 
-
 int main() {
   std::cout << "Start Testing Sparse Matrix Multiplication Layer" << std::endl;
   test_smm_relu();
+  test_smm_softmax();
 }
--- test/test_smm.py
+++ test/test_smm.py
@@ -33,3 +33,52 @@ dump("gb", gb)
 
 dump("update_w", w - lr*gw)
 dump("update_b", b - lr*gb)
+
+
+def stable_softmax(x_):
+    exps = np.exp(x_ - np.max(x_))
+    sum_ = np.sum(exps)
+    if sum_ > 0:
+        return exps / sum_
+    else:
+        return exps
+
+
+def delta_cross_entropy(x_, y):
+    m = y.shape[0]
+    grad = stable_softmax(x_)
+    grad[range(m), y] -= 1
+    grad = grad / m
+    return grad
+
+
+def cross_entropy(X, y):
+    m = y.shape[0]
+    p = stable_softmax(X)
+    log_likelihood = -np.log(p[range(m), y])
+    loss = np.sum(log_likelihood) / m
+    return loss
+
+M = 1
+D = 8  # I_
+N = 4  # O_
+print("Testing SoftMax forward")
+np.random.seed(1216)
+x = np.random.normal(size=(M, D))
+w = np.random.normal(size=(D, N))
+b = np.random.normal(size=(1, N))
+o = x @ w + b
+print("size_type I={}, O={};".format(D, N))
+dump("x", x)
+dump("w", w)
+dump("b", b)
+
+dump("o", stable_softmax(o))
+y = np.array([np.random.choice(a=N, size=1) for _ in range(M)])
+dump("y", y)
+grad = delta_cross_entropy(o, y)
+dump("g", grad)
+loss = cross_entropy(o, y)
+print("T loss_ = {};".format(loss))
+
+
